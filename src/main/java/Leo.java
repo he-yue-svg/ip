80bfc8c0817da88802fea3beac0760658c4f12a9
he-yue-svg <178892600+he-yue-svg@users.noreply.github.com>
2025-08-20 16:10:36 +0800

    Ability to mark tasks as done

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Leo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] array = new String[100];
+        Task[] array = new Task[100];
         int index = 0;
         String line = "----------------------------------------";
         System.out.println(line+ "\n" + "Hello I'm Leo\n" +
@@ -13,20 +13,42 @@ public class Leo {
             String input = sc.nextLine(); // reads input
 
             if (input.equals("bye")) {
-                System.out.println(line + "\n" + "Bye. Hope to see you again soon!\n" + line);
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!\n" + line);
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list");
                 for (int i=0; i<index; i++) {
-                    System.out.println(i+1 + ". " + array[i]);
+                    System.out.println(i+1 + ".[" + array[i].getStatusIcon()
+                            + "] " + array[i].getDescription());
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark")) {
+                String[] parts = input.split(" ");
+                int value = Integer.parseInt(parts[parts.length-1]);
+                array[value-1].markAsDone();
+                System.out.println("Nice, I've marked this task as done:");
+                System.out.println("[" + array[value-1].getStatusIcon()
+                        + "] " + array[value-1].getDescription());
+                System.out.println(line);
+            } else if (input.startsWith("unmark")) {
+                String[] parts = input.split(" ");
+                int value = Integer.parseInt(parts[parts.length-1]);
+                array[value-1].markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + array[value-1].getStatusIcon()
+                        + "] " + array[value-1].getDescription());
+                System.out.println(line);
             } else {
-                array[index] = input;
+                Task task = new Task(input);
+                array[index] = task;
                 index++;
-                System.out.println(line + "\n" + "added: " +  input + "\n" + line);
+                System.out.println(line);
+                System.out.println("added: " +  input + "\n" + line);
             }
         }
 
         sc.close();
     }
+
 }
