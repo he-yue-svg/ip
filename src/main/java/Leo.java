0b95ef321d4abfe1d7c3eb7d9fa8c1caf498c393
he-yue-svg <178892600+he-yue-svg@users.noreply.github.com>
2025-08-20 15:08:49 +0800

    Renamed file, included Greet and Exit

---
+++ src/main/java/Leo.java
@@ -0,0 +1,8 @@
+public class Leo {
+    public static void main(String[] args) {
+        String line = "----------------------------------------";
+        System.out.println(line+ "\n" + "Hello I'm Leo\n" +
+                "What can I do for you?\n" + line + "\n" +
+                "Bye. Hope to see you again soon!\n" + line);
+    }
+}
