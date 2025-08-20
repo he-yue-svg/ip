4ba1f09107fab847f8d6c8fb0df4e987e70d56ff
he-yue-svg <178892600+he-yue-svg@users.noreply.github.com>
2025-08-20 15:25:22 +0800

    Echoed the input

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Leo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String line = "----------------------------------------";
         System.out.println(line+ "\n" + "Hello I'm Leo\n" +
-                "What can I do for you?\n" + line + "\n" +
-                "Bye. Hope to see you again soon!\n" + line);
+                "What can I do for you?\n" + line);
+
+        while (true) {
+            String input = sc.nextLine(); // reads input
+
+            if (input.equals("bye")) {
+                System.out.println(line + "\n" + "Bye. Hope to see you again soon!\n" + line);
+                break;
+            } else {
+                System.out.println(line + "\n" + input + "\n" + line);
+            }
+        }
+
+        sc.close();
     }
 }
