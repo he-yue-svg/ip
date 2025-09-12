73adb086b405ab6fac99b718f368880d33889fdf
he-yue-svg <178892600+he-yue-svg@users.noreply.github.com>
2025-09-12 20:59:08 +0800

    Improve code quality

--- src/main/java/leo/Ui.java
+++ src/main/java/leo/Ui.java
@@ -17,26 +17,30 @@ public class Ui {
     }
 
     public String goodBye() {
+        String prompt = "Bye. Hope to see you again soon!";
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        System.out.println(prompt);
+        System.out.println(line);
         sc.close();
-        return "Bye. Hope to see you again soon!";
+        return prompt;
     }
 
     public String showMarked(String str) {
+        String prompt = "Nice! I've marked this task as done:";
         System.out.println(line);
-        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(prompt);
         System.out.println(str);
         System.out.println(line);
-        return ("Nice! I've marked this task as done:" + "\n" + str);
+        return (prompt + "\n" + str);
     }
 
     public String showUnmarked(String str) {
+        String prompt = "Nice! I've marked this task as not done:";
         System.out.println(line);
-        System.out.println("Nice! I've marked this task as not done:");
+        System.out.println(prompt);
         System.out.println(str);
         System.out.println(line);
-        return ("Nice! I've marked this task as not done:" + "\n" + str);
+        return (prompt + "\n" + str);
     }
 
     public String readCommand() {
@@ -46,37 +50,43 @@ public class Ui {
     }
 
     public String taskAdded(String str, int size) {
+        String prompt = "Got it. I've added this task:";
+        String list = "Now you have " + size + " tasks in the list.";
         System.out.println(line);
-        System.out.println("Got it. I've added this task:");
+        System.out.println(prompt);
         System.out.println(str);
-        System.out.println("Now you have " + size + " tasks in the list.");
+        System.out.println(list);
         System.out.println(line);
-        return ("Got it. I've added this task:" + "\n" + str + "\n" + "Now you have " + size + " tasks in the list.");
+        return (prompt + "\n" + str + "\n" + list);
     }
 
     public String taskDeleted(String str, int size) {
+        String prompt = "Noted. I've removed this task:";
+        String list = "Now you have " + size + " tasks in the list.";
         System.out.println(line);
-        System.out.println("Noted. I've removed this task:");
+        System.out.println(prompt);
         System.out.println(str);
-        System.out.println("Now you have " + size + " tasks in the list.");
+        System.out.println(list);
         System.out.println(line);
-        return ("Noted. I've removed this task:" + "\n" + str + "\n" + "Now you have " + size + " tasks in the list.");
+        return (prompt + "\n" + str + "\n" + list);
     }
 
     public String iterate(String str) {
+        String prompt = "Here are the tasks in your list";
         System.out.println(line);
-        System.out.println("Here are the tasks in your list");
+        System.out.println(prompt);
         System.out.println(str);
         System.out.println(line);
-        return ("Here are the tasks in your list" + "\n" + str);
+        return (prompt + "\n" + str);
     }
 
     public String find(String str) {
+        String prompt = "Here are the tasks in your list";
         System.out.println(line);
-        System.out.println("Here are the tasks in your list");
+        System.out.println(prompt);
         System.out.println(str);
         System.out.println(line);
-        return ("Here are the tasks in your list" + "\n" + str);
+        return (prompt + "\n" + str);
     }
 
     public String showError(Exception e) {
